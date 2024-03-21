142876a4c73ccc00156db7f9d79d6fec0b17ea49
kobam <takuya.2441okc@gmail.com>
2024-03-21 23:16:35 +0900

    [Remote Procedure Call] サーバ・クライアント要求応答の実装

--- dev-containers/src/client.ts
+++ dev-containers/src/client.ts
@@ -6,6 +6,16 @@ import * as Log4js from "log4js";
 const PORT: number = 50000;
 const HOST: string = "localhost"
 
+/**
+ * request用のインターフェース
+ */
+interface Request {
+    method: string,
+    params: number[] | string[],
+    param_types: string[],
+    id: string   
+}
+
 // 設定ファイルの読み込み
 // const configure = Config.util.loadFileConfigs(Path.join(__dirname, "config")).log4js;
 Log4js.configure("./config/development.json");
@@ -15,11 +25,17 @@ const logger: Log4js.Logger = Log4js.getLogger("access");
 // ソケットの作成
 const client: net.Socket = new net.Socket();
 
+const req: Request = {
+    method: "reverse",
+    params: ["abcdef"],
+    param_types: ["string"],
+    id: "1"
+}
 // サーバに接続
 client.connect(PORT, HOST, () => {
     logger.info("Connect started");
-    // メッセージの送信
-    client.write("test message");
+    // JSON文字列に変換しメッセージを送信
+    client.write(JSON.stringify(req));
 });
 
 // サーバからのデータを受信する
@@ -38,3 +54,4 @@ client.on("close", () => {
 client.on("error", (err: Error) => {
     logger.warn("Error: ", err);
 });
+
--- dev-containers/src/server.py
+++ dev-containers/src/server.py
@@ -1,58 +1,93 @@
-# socketとosモジュールをインポートします
 import socket
-import os
+import json
+import math
 
-# UNIXソケットをストリームモードで作成します
+# 10進数 x を最も近い整数に切り捨て、その結果を整数で返す
+def floor(x):
+    return (int) (x - (x % 1))
+
+# 
+def nroot(n, x):
+    return math.pow(x, 1/n)
+
+# 入力文字列の逆である文字列を返す
+def reverse(str):
+    reverse_str = ''
+    for i in range(len(str)):
+        reverse_str += str[len(str) - i - 1]
+    return reverse_str
+
+# 引数のJSON文字列を元に特定の関数を呼び出す
+def rpcFunction(json):
+    method = json['method']
+    params = json['params']
+    print('===== Pamrams:  ==> {}'.format(params[0]))
+    
+    response = ''
+    if method == 'floor':
+        response = {
+            "results": floor(params[0]),
+            "result_type": "int",
+            "id": json['id']
+        }
+    elif method == 'nroot': 
+        response = {
+            "results": nroot(params[0], params[1]),
+            "result_type": "double",
+            "id": json['id']
+        }
+    elif method == 'reverse':
+        response = {
+            "results": reverse(params[0]),
+            "result_type": "string",
+            "id": json['id']
+        }
+        
+    print('===== Response: <== {} '.format(response['results']))
+    return response
+
+# AF_INETをストリームモードで作成 (AF_UNIXではhostとportの指定ができない)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# このサーバが接続を待つUNIXソケットのパスを設定します
-#server_address = '/workspaces/Recursion/dev-containers/socket_file'
 # サーバのホストとポート番号を設定
 server_address = ('localhost', 50000)
 
-# # 以前の接続が残っていた場合に備えて、サーバアドレスをアンリンク（削除）します
-# try:
-#     os.unlink(server_address)
-# # サーバアドレスが存在しない場合、例外を無視します
-# except FileNotFoundError:
-#     pass
-
 print('Starting up on {}:{}'.format(*server_address))
 
-# サーバアドレスにソケットをバインド（接続）します
+# サーバアドレスにソケットをバインド(接続)
 sock.bind(server_address)
 
-# ソケットが接続要求を待機するようにします
+# ソケットが接続要求を待機するように設定
 sock.listen(1)
 
-# 無限ループでクライアントからの接続を待ち続けます
+# 無限ループでクライアントからの接続を待機
 while True:
-    # クライアントからの接続を受け入れます
+    # クライアントからの接続を許可
     connection, client_address = sock.accept()
     try:
-        print('connection from', client_address)
+        print('Connection from: ', client_address)
 
-        # ループが始まります。これは、サーバが新しいデータを待ち続けるためのものです。
+        # ループ開始(サーバが新しいデータの待機を継続する)
         while True:
-            # ここでサーバは接続からデータを読み込みます。
-            # 16という数字は、一度に読み込むデータの最大バイト数です。
+            # データの受信
             data = connection.recv(1024)
 
             # 受け取ったデータはバイナリ形式なので、それを文字列に変換します。
             # 'utf-8'は文字列のエンコーディング方式です。
             data_str =  data.decode('utf-8')
-
-            # 受け取ったデータを表示します。
-            print('Received data: ' + data_str)
-
-            # もしデータがあれば（つまりクライアントから何かメッセージが送られてきたら）以下の処理をします。
+            
+            try:
+                # JSON文字列に変換する
+                data_json = json.loads(data_str)
+                # 受け取ったデータを表示します。
+                print('Received data: ' + data_json["method"])
+            except Exception:
+                pass
+            
+            # クライアントからメッセージを受信した場合
             if data:
-                # 受け取ったメッセージを処理します。
-                response = 'Processing: ' + data_str
-
-                # 処理したメッセージをクライアントに送り返します。
-                # ここでメッセージをバイナリ形式（エンコード）に戻してから送信します。
-                connection.sendall(response.encode())
+                # RPC関数を用いてメッセージを送信 (strをencodeする)
+                connection.sendall(json.dumps(rpcFunction(data_json)).encode())
 
             # クライアントからデータが送られてこなければ、ループを終了します。
             else:
